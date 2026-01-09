9d559d324513e936cbe9348b8b252d56634527d9
Elkin <elkinboterooficial@gmail.com>
2026-01-09 15:33:21 -0500

    delete requeriments.txt y cambio de version de python

--- render_build.sh
+++ render_build.sh
@@ -2,9 +2,15 @@
 # exit on error
 set -o errexit
 
+# Build frontend
 npm install
 npm run build
 
-pipenv install
+# Install pipenv if not available
+pip install pipenv
 
+# Install Python dependencies from Pipfile
+pipenv install --deploy
+
+# Run database migrations using pipenv script
 pipenv run upgrade
